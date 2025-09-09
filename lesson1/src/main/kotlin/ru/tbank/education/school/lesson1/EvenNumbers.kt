598da07fae69dd6b090e9d1b7deb4fc3880e89d6
V9kerNiene <garry_gan@outlook.com>
2025-09-09 13:31:16 +0300

    SC

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -4,5 +4,11 @@ package ru.tbank.education.school.lesson1
  * Сумма четных чисел.
  */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var u = 0
+    for (i in numbers) {
+        if (i%2==0) {
+            u += i
+        }
+    }
+    return u
 }
